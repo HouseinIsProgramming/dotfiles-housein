b0090cd8f2b95b02a8acf821aeba495e310204a5
Housein Abo Shaar <76689341+GogoIsProgramming@users.noreply.github.com>
2025-09-07 15:53:25 +0200

    refactoring and tree stiter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -11,6 +11,41 @@ return {
 				highlight = { enable = true },
 				indent = { enable = true },
 				auto_install = true,
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						node_incremental = "<C-o>",
+						scope_incremental = false,
+						node_decremental = "<C-i>",
+					},
+				},
+				textobjects = {
+					move = {
+						enable = true,
+						set_jumps = true, -- whether to set jumps in the jumplist
+						goto_next_start = {
+							["]m"] = "@function.outer",
+							["]f"] = "@function.outer",
+							["]]"] = { query = "@class.outer", desc = "Next class start" },
+						},
+						goto_previous_start = {
+							["[m"] = "@function.outer",
+							["[f"] = "@function.outer",
+							["[["] = { query = "@class.outer", desc = "Previous class start" },
+						},
+					},
+					select = {
+						enable = true,
+						lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+						keymaps = {
+							-- You can use the capture groups defined in textobjects.scm
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+						},
+					},
+				},
 			})
 		end,
 	},
