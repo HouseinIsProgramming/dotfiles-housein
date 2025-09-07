9535eb92fa42751edab7e1e51681b4b8aaca4cc6
Housein Abo Shaar <76689341+GogoIsProgramming@users.noreply.github.com>
2025-09-07 15:11:06 +0200

    theme

--- .config/nvim/lua/plugins/lackluster.lua
+++ .config/nvim/lua/plugins/lackluster.lua
@@ -1,15 +1,15 @@
 -- example lazy.nvim install setup
 return {
-  "slugbyte/lackluster.nvim",
-  lazy = false,
-  priority = 1000,
-  init = function()
-    -- vim.cmd.colorscheme("lackluster")
-    -- vim.cmd.colorscheme("lackluster-night") -- my favorite
-    -- vim.cmd.colorscheme("lackluster-mint")
-    --
-    vim.cmd([[
+	"slugbyte/lackluster.nvim",
+	lazy = false,
+	priority = 1000,
+	init = function()
+		-- vim.cmd.colorscheme("lackluster")
+		-- vim.cmd.colorscheme("lackluster-night") -- my favorite
+		-- vim.cmd.colorscheme("lackluster-mint")
+		--
+		vim.cmd([[
       hi Folded guibg=#171717 guifg=#F1F5F9
       ]])
-  end,
+	end,
 }
