8b85b3f4f2b2aa7ba3cc0196ddc4998f59ef2437
Housein Abo Shaar <76689341+GogoIsProgramming@users.noreply.github.com>
2025-09-10 20:57:55 +0200

    timetracker

--- .config/scripts/timetrack.sh
+++ .config/scripts/timetrack.sh
@@ -17,8 +17,8 @@ selected=$(printf "%s\n" "${CATEGORIES[@]}" | sk --margin 10% --color="bw")
 
 if [[ $selected == "STOP" ]]; then
   timew stop
-  tmux set -g status-right "#[fg=black,bg=red,bold] $selected "
+  # tmux set -g status-right "#[fg=black,bg=red,bold] $selected "
 else
   timew start "$selected"
-  tmux set -g status-right "#[fg=black,bg=green,bold] $selected "
+  # tmux set -g status-right "#[fg=black,bg=green,bold] $selected "
 fi
