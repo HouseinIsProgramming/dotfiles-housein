bf2848406fd4be494149ece242be93ba0e2abbb6
Housein Abo Shaar <76689341+GogoIsProgramming@users.noreply.github.com>
2025-09-10 19:33:59 +0200

    added time tracker

---
+++ .config/scripts/timetrack.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# timetrack.sh
+# A simple time tracking script
+#
+
+CATEGORIES=(
+  "VENDURE"
+  "WORKFLOW"
+  "PROGRAMMING"
+  "LEARNING"
+  "WASTED"
+  "STOP"
+)
+
+selected=$(printf "%s\n" "${CATEGORIES[@]}" | sk --margin 10% --color="bw")
+
+if [[ $selected == "STOP" ]]; then
+  timew stop
+  tmux set -g status-right "#[fg=black,bg=red,bold] $selected "
+else
+  timew start "$selected"
+  tmux set -g status-right "#[fg=black,bg=green,bold] $selected "
+fi
