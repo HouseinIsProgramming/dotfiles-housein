166207c0707856b1b338bc940146e6146efc6dd1
Housein Abo Shaar <76689341+GogoIsProgramming@users.noreply.github.com>
2025-09-20 02:36:40 +0200

    rewrite start

--- .config/nvim_bak/lua/plugins/indentmini.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvimdev/indentmini.nvim",
-  event = "BufEnter",
-  config = function()
-    require("indentmini").setup()
-    vim.cmd("highlight IndentLine guifg=#404040")
-    vim.cmd("highlight IndentLineCurrent guifg=#737373")
-  end,
-}
