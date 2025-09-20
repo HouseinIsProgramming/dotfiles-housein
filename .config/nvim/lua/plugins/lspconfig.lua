a70f5f91ad93fc884abcbd3b8ae1c11f2ca7e201
Housein Abo Shaar <76689341+GogoIsProgramming@users.noreply.github.com>
2025-09-20 04:57:35 +0200

    add auto cmds to lsp

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -46,11 +46,42 @@ return {
           -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
           if client:supports_method('textDocument/completion') then
             -- Optional: trigger autocompletion on EVERY keypress. May be slow!
-            local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-            ---@diagnostic disable-next-line: need-check-nil
-            client.server_capabilities.completionProvider.triggerCharacters = chars
+            vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = false })
+          end
 
-            vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+          local opts = { buffer = args.buf }
+
+          -- Open the diagnostic under the cursor in a float window
+          vim.keymap.set("n", "<leader>d", function()
+            vim.diagnostic.open_float({
+              border = "single",
+            })
+          end, opts)
+
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+          -- Source actions keymap
+          vim.keymap.set(
+            "n",
+            "<leader>ls",
+            function()
+              vim.lsp.buf.code_action({
+                context = {
+                  only = { "source" }, -- Request all "source" actions
+                  diagnostics = {},
+                },
+              })
+            end,
+            opts
+          )
+          if not client:supports_method('textDocument/willSaveWaitUntil')
+              and client:supports_method('textDocument/formatting') then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+              end,
+            })
           end
         end,
       })
