20011615f8f8ba2abff1f7f2e77d40f1590cbe8b
Housein Abo Shaar <76689341+GogoIsProgramming@users.noreply.github.com>
2025-12-22 12:10:28 +0100

    remove chard port

--- .config/nvim_chad/init.lua
+++
@@ -1,9 +0,0 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
-
-require("config.lazy")
-require("config.options")
-require("config.keymaps")
-require("config.autocmds")
